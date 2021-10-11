3598c616fc8cde09ecdd40d999327c4ab2a5e3d3
AdamKuemmel <Adamkuemmel@gmail.com>
2021-10-11 12:09:01 -0600

    gitignore

--- src/components/pages/Lightbox.js
+++ src/components/pages/Lightbox.js
@@ -3,6 +3,7 @@ import Lightbox from "react-image-lightbox";
 import "react-image-lightbox/style.css"; // This only needs to be imported once in your app
 import resume from "../../images/sampleResume.png";
 import dowloadRES from "../../images/AKresume.pdf";
+import "mdb-react-ui-kit/dist/css/mdb.min.css";
 
 const images = [resume];
 const styles = {
